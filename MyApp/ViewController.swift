53878199efd58bdc2f3043ed16ee9c9956efd2cc
Aleksei Naumyshev <naumyshev@yandex.ru>
2025-12-22 20:58:51 +0300

    Initial Commit

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Aleksei Naumyshev on 22.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
